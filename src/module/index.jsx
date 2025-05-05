59d18ce5478061f6e76dd6df496a17fa42a1089e
Tomáš Pokorný <tompokorny@seznam.cz>
2025-05-05 21:33:42 +0200

    feat - create training app

---
+++ src/module/index.jsx
@@ -0,0 +1,36 @@
+import { Module } from "asab_webui_components";
+
+import { TableScreen } from './TableScreen.jsx';
+import {DetailScreen} from "./Detail";
+
+export default class TableApplicationModule extends Module {
+	constructor(app, name) {
+		super(app, "TableApplicationModule");
+
+		app.Router.addRoute({
+			path: "/",
+			end: false,
+			name: 'Table',
+			component: TableScreen,
+		});
+
+		app.Router.addRoute({
+			path: "/detail/:id",
+			end: true, // exact match
+			name: 'Detail',
+			component: DetailScreen,
+		});
+
+		app.Navigation.addItem({
+			name: "Table",
+			icon: 'bi bi-table',
+			url: "/",
+		});
+
+		app.Navigation.addItem({
+			name: "Detail",
+			icon: 'bi bi-pen',
+			url: "/detail",
+		});
+	}
+}
